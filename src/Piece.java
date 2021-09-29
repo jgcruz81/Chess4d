9316e0b0e169d99f2941e915e28a159edb2a3393
Juan <juangcruz81@gmail.com>
2021-09-29 18:57:57 -0400

    added the basic classes

--- src/Piece.java
+++ src/Piece.java
@@ -1,6 +1,32 @@
-public class Piece {
-    public static void main(String[] args){
-        System.out.println("Test");
+public abstract class Piece {
 
+    private boolean killed = false;
+    private boolean white = false;
+
+    public Piece(boolean white)
+    {
+        this.setWhite(white);
     }
+
+    public boolean isWhite()
+    {
+        return this.white;
+    }
+
+    public void setWhite(boolean white)
+    {
+        this.white = white;
+    }
+
+    public boolean isKilled()
+    {
+        return this.killed;
+    }
+
+    public void setKilled(boolean killed)
+    {
+        this.killed = killed;
+    }
+
+    public abstract boolean canMove(Board board, Spot start, Spot end);
 }
